a7ae8fe90042d6b9b45c7c2daa549bbf896d49dd
Shih Wei <loneswei@gmail.com>
2018-01-17 14:01:31 +0800

    Implemented PauseMenu(Pop-up window)

---
+++ Assignment02/Assets/Scripts/Controls.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour {
+
+    [SerializeField]
+    GameObject pauseButton;
+
+    [SerializeField]
+    GameObject pauseMenu;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public void Pause()
+    {
+        pauseButton.SetActive(false);
+        pauseMenu.SetActive(true);
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assignment02/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    [SerializeField]
+    GameObject pauseButton;
+
+    [SerializeField]
+    GameObject pauseMenu;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        pauseButton.SetActive(true);
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
