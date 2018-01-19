05c108a949ee45b4e6a5b27cad8ab97557d29c04
DELFHIN\Delfhin <aminusdolphin@gmail.com>
2018-01-20 00:38:12 +0800

    Added in a skeleton of sounds effect slider, 1 script to save/load settings.

--- Assignment02/Assets/Scripts/MainMenu/OptionScreen.cs
+++ Assignment02/Assets/Scripts/MainMenu/OptionScreen.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionScreen : MonoBehaviour {
 
@@ -9,6 +10,10 @@ public class OptionScreen : MonoBehaviour {
 
     [SerializeField]
     GameObject optionScreen;
+
+    public AudioSource soundEffectsSource;
+    public Slider soundEffectsVolume_Slider;
+    OptionSettings optionSettings;
 
 	// Use this for initialization
 	void Start () {
@@ -20,6 +25,28 @@ public class OptionScreen : MonoBehaviour {
         playButton.SetActive(true);
         optionButton.SetActive(true);
         optionScreen.SetActive(false);
+    }
+
+    public void OnEnable()
+    {
+        optionSettings = new OptionSettings();
+
+        soundEffectsVolume_Slider.onValueChanged.AddListener(delegate { SoundEffectsVolumeChange(); });
+    }
+
+    public void SoundEffectsVolumeChange()
+    {
+        soundEffectsSource.volume = optionSettings.soundEffects_Volume = soundEffectsVolume_Slider.value;
+    }
+
+    public void SaveSettings()
+    {
+
+    }
+
+    public void LoadSettings()
+    {
+
     }
 
 	// Update is called once per frame
---
+++ Assignment02/Assets/Scripts/MainMenu/OptionSettings.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class OptionSettings{
+
+    public float soundEffects_Volume;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
