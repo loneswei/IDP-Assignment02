b016e1fe23cd59d846649a8111a1b45a3bbdbf39
DELFHIN\Delfhin <aminusdolphin@gmail.com>
2018-01-21 22:08:21 +0800

    Added in background music for main menu. Updated Options, able to save the volume of music even if it changes scene.

--- Assignment02/Assets/Scripts/MainMenu/OptionScreen.cs
+++ Assignment02/Assets/Scripts/MainMenu/OptionScreen.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
 
 public class OptionScreen : MonoBehaviour {
 
@@ -11,46 +12,34 @@ public class OptionScreen : MonoBehaviour {
     [SerializeField]
     GameObject optionScreen;
 
-    public AudioSource soundEffectsSource;
-    public Slider soundEffectsVolume_Slider;
-    OptionSettings optionSettings;
+    public Button applyButton;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+    public Slider soundEffectsVolume_Slider;
+    public Slider musicVolume_Slider;
+
+    private AudioSource musicSource;
+
+    void Start()
+    {
+        musicSource = GetComponent<AudioSource>();
+        musicVolume_Slider.value = PlayerPrefs.GetFloat("musicVol", 1);
+    }
+
+    void Update()
+    {
+        musicSource.volume = musicVolume_Slider.value;
+    }
+
+    public void Apply()
+    {
+        PlayerPrefs.SetFloat("musicVol", musicVolume_Slider.value);
+        Debug.Log("Saved");
+    }
+
     public void Back()
     {
         playButton.SetActive(true);
         optionButton.SetActive(true);
         optionScreen.SetActive(false);
     }
-
-    public void OnEnable()
-    {
-        optionSettings = new OptionSettings();
-
-        soundEffectsVolume_Slider.onValueChanged.AddListener(delegate { SoundEffectsVolumeChange(); });
-    }
-
-    public void SoundEffectsVolumeChange()
-    {
-        soundEffectsSource.volume = optionSettings.soundEffects_Volume = soundEffectsVolume_Slider.value;
-    }
-
-    public void SaveSettings()
-    {
-
-    }
-
-    public void LoadSettings()
-    {
-
-    }
-
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
