a14a278ae83999f2389bf0c421b835ab957dc220
DELFHIN\Delfhin <aminusdolphin@gmail.com>
2018-01-23 10:59:23 +0800

    Updated Audio on gamescene. There are sounds for every button pressed (Mouse click sound), and background music. No sound for changing of weapons yet.

--- Assignment02/Assets/Scripts/GameScene/OptionMenu.cs
+++ Assignment02/Assets/Scripts/GameScene/OptionMenu.cs
@@ -9,38 +9,51 @@ public class OptionMenu : MonoBehaviour {
     GameObject optionMenu, quitButton;
 
     [SerializeField]
-    GameObject gunShotSound/*, gunReloadSound*/;
+    GameObject mouseClickSound, gunShotSound, gunReloadSound;
+
+    [SerializeField]
+    GameObject backgroundMusic;
 
     public Slider soundEffects_Slider;
+    public Slider backgroundMusic_Slider;
     public Button applyButton;
 
-    private AudioSource gunShotSource/*, gunReloadSource*/;
-    private float volume;
+    private AudioSource mouseClickSource, gunShotSource, gunReloadSource;
+    private AudioSource backgroundMusicSource;
 
 	// Use this for initialization
 	void Start ()
     {
+        mouseClickSource = mouseClickSound.GetComponent<AudioSource>();
         gunShotSource = gunShotSound.GetComponent<AudioSource>();
-        //gunReloadSource = gunReloadSound.GetComponent<AudioSource>();
+        gunReloadSource = gunReloadSound.GetComponent<AudioSource>();
+        backgroundMusicSource = backgroundMusic.GetComponent<AudioSource>();
 
-        soundEffects_Slider.value = PlayerPrefs.GetFloat("sfxVol", 1); ;
+
+        soundEffects_Slider.value = PlayerPrefs.GetFloat("sfxVol", 1);
+        backgroundMusic_Slider.value = PlayerPrefs.GetFloat("musicVol", 1);
 	}
 
     public void Apply()
     {
         PlayerPrefs.SetFloat("sfxVol", soundEffects_Slider.value);
+        PlayerPrefs.SetFloat("musicVol", backgroundMusic_Slider.value);
     }
 
     // Update is called once per frame
     void Update()
     {
+        mouseClickSource.volume = soundEffects_Slider.value;
         gunShotSource.volume = soundEffects_Slider.value;
-        //gunReloadSource.volume = soundEffects_Slider.value;
+        gunReloadSource.volume = soundEffects_Slider.value;
+
+        backgroundMusicSource.volume = backgroundMusic_Slider.value;
     }
 
     public void CloseOption()
     {
         soundEffects_Slider.value = PlayerPrefs.GetFloat("sfxVol", 1);
+        backgroundMusic_Slider.value = PlayerPrefs.GetFloat("musicVol", 1);
 
         optionMenu.SetActive(false);
         quitButton.SetActive(true);
