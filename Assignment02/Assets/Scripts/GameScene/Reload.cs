8e737af736cb95cdbf382d3ceb5ea74f5edc9f6d
DELFHIN\Delfhin <aminusdolphin@gmail.com>
2018-01-24 00:19:10 +0800

    Fixed Audio. Reload sound will not play if it's full but it will still play if the bullet is empty and no more magazine to reload.

--- Assignment02/Assets/Scripts/GameScene/Reload.cs
+++ Assignment02/Assets/Scripts/GameScene/Reload.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Reload : MonoBehaviour {
 
+    [SerializeField]
+    GameObject soundEffectSound;
+    private AudioSource soundEffectSource;
+
 	// Use this for initialization
 	void Start(){}
 	// Update is called once per frame
@@ -11,6 +15,9 @@ public class Reload : MonoBehaviour {
 
     public void ReloadCurrBullets()
     {
+        soundEffectSource = soundEffectSound.GetComponent<AudioSource>();
+        soundEffectSource.volume = PlayerPrefs.GetFloat("sfxVol", 1);
+
         BulletInventory bullets = GetComponent<BulletInventory>();
         PlayerWeapons weapons = GetComponent<PlayerWeapons>();
         if (Time.timeScale == 0.0f) // if  paused, return
@@ -26,11 +33,15 @@ public class Reload : MonoBehaviour {
 
                 if (BulletsToLoad <= bullets.GetPriTotalBullets())
                 {
+                    soundEffectSource.Play();
+
                     bullets.SetPriCurrBullets(bullets.GetPriMaxBullets());
                     bullets.AddPriTotalBullets(-BulletsToLoad);
                 }
                 else
                 {
+                    soundEffectSource.Play();
+
                     bullets.SetPriCurrBullets(bullets.GetPriCurrBullets() + bullets.GetPriTotalBullets());
                     bullets.AddPriTotalBullets(-(bullets.GetPriTotalBullets()));
                 }
@@ -38,6 +49,7 @@ public class Reload : MonoBehaviour {
         }
         else if (weapons.GetCurrWeaponType() == "Secondary")
         {
+
             // Check if need to reload
             if (bullets.GetSecCurrBullets() < bullets.GetSecMaxBullets())
             {
@@ -46,11 +58,15 @@ public class Reload : MonoBehaviour {
 
                 if (BulletsToLoad <= bullets.GetSecTotalBullets())
                 {
+                    soundEffectSource.Play();
+
                     bullets.SetSecCurrBullets(bullets.GetSecMaxBullets());
                     bullets.AddSecTotalBullets(-BulletsToLoad);
                 }
                 else
                 {
+                    soundEffectSource.Play();
+
                     bullets.SetSecCurrBullets(bullets.GetSecCurrBullets() + bullets.GetSecTotalBullets());
                     bullets.AddSecTotalBullets(-(bullets.GetSecTotalBullets()));
                 }
