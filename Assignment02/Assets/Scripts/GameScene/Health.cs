b9c85828f7764300753252eeda3eff502b88256d
Shih Wei <loneswei@gmail.com>
2018-01-23 21:50:59 +0800

    Added HealthBar for enemy :astonished:

--- Assignment02/Assets/Scripts/GameScene/Health.cs
+++ Assignment02/Assets/Scripts/GameScene/Health.cs
@@ -1,20 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour {
 
     [SerializeField]
-    float healthPoints = 10.0f;
+    float startHealth = 10.0f;
+    float healthPoints;
 
+    public Image healthBar;
 	// Use this for initialization
 	void Start () {
-		
+        healthPoints = startHealth;
 	}
 	
     public void AddHealth(float amount)
     {
         healthPoints += amount;
+
+        if (gameObject.name.Contains("Enemy"))
+            healthBar.fillAmount = healthPoints / startHealth;
 
         // Die
         if (healthPoints <= 0)
